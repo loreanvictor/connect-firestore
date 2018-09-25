aabf2e3b0f9554729899d44b79a8e7d97cf917f6
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2018-09-25 09:30:44 +0200

    complete setup guide

--- index.js
+++ index.js
@@ -29,7 +29,15 @@ module.exports.platform = {
     }
   },
   hints: {
-    setup: "This is how you setup CONNECT firestore.",
+    setup:
+`First, you need a Google Firestore database. Go to your Google Firebase console, and create one.
+Then,
+1) go to your project's settings (the gear icon near <span class="hl-blue">Project Overview</span>, click it and you see settings) <br>
+2) go to <span class="hl-blue">Service accounts</span><br>
+3) press the <span class="hl-blue">Generate new private key</span> button<br>
+4) go to <span class="hl-blue">Vault</span> section of panel, and create a new key, for example named <span class="hl-teal">firestore.json</span><br>
+5) copy the content of the private key you generated in the content field of the new key
+6) add a sample config like this. the project field should be the same as <span class="hl-teal">project_id</span> field of your key. the keyfile field is the name of the key you created in your vault.`,
     sampleConfig: {
       project: "your-firestore-project-name",
       keyfile: "firestore.json"
